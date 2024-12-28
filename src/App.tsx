e13ec4c2cd4ad5a360ecf2726b7943cdf4951504
manojkumarkasiviswanathan <manojkumar.kasiviswanathan@gmail.com>
2024-12-28 12:27:19 +0000

    feat: added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Header from './components/Header/Header';
-
+import About from './components/About';
 export default function App() {
     return (
         <div id="portfolio-content" className="min-h-screen bg-gray-950">
             <Header/>
+            <About />
             <main className="relative">
                 <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top,#1F2937,transparent)] -z-10"/>
             </main>
